3da93d245128723f3b2e2b20165f15725f104b64
omid-joukar <58634223+omid-joukar@users.noreply.github.com>
2020-07-29 13:00:17 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/**
+ * Created by KPS on 7/29/2020.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        String str1 = "example.com", str2 = "Example.com";
+        StringBuffer strbuf = new StringBuffer(str1);
+
+        System.out.println("Comparing "+str1+" and "+strbuf+": " + str1.contentEquals(strbuf));
+
+        System.out.println("Comparing "+str2+" and "+strbuf+": " + str2.contentEquals(strbuf));
+
+    }
+}
+
